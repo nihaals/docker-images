3b91f655d6213c1fce34a774976f46364bd41e9f
Nihaal Sangha <18350092+nihaals@users.noreply.github.com>
2020-07-04 23:00:56 +0100

    Improved readability of generate-matrix.py

--- generate-matrix.py
+++ generate-matrix.py
@@ -3,12 +3,19 @@ from __future__ import annotations
 import json
 import os
 from pathlib import Path
-from typing import Dict, List
+from typing import Dict, List, TypedDict
 
-root_path = Path(__file__).parent.joinpath('images')
+ROOT_PATH = Path(__file__).parent
+ROOT_IMAGES_PATH = ROOT_PATH.joinpath('images')
+
+
+class InfoDict(TypedDict):
+    aliases: Dict[str, str]
 
 
 class Version:
+    """Represents a tag of an image."""
+
     def __init__(self, path: str, tags: List[str]) -> None:
         self.name = path.split('/')[1].lower()
         self.path = path
@@ -16,6 +23,8 @@ class Version:
 
 
 class Image:
+    """Represents an image name. Contains `Version`s (tags)."""
+
     def __init__(self, path: str, versions: List[Version]) -> None:
         self.path = path
         self.name = self.path.title()
@@ -24,22 +33,29 @@ class Image:
 
     @classmethod
     def from_path(cls, path: str) -> Image:
-        image_path = root_path.joinpath(path)
+        """Create an `Image` from its path e.g. `'playground'`."""
+        image_path = ROOT_IMAGES_PATH.joinpath(path)
         if image_path.joinpath('info.json').exists() is False:
+            # Check if image directory has an `info.json`
             raise ValueError(f"Invalid file strucutre for {path}. No info.json")
-        with open(root_path.joinpath(path, 'info.json')) as fp:
-            info_json = json.load(fp)
-        aliases: Dict[str, str] = info_json['aliases']
-        sub_paths: List[str] = []
+        with open(ROOT_IMAGES_PATH.joinpath(path, 'info.json')) as fp:
+            info_json: InfoDict = json.load(fp)
+
+        sub_paths: List[str] = []  # List of paths of `Version`s
         for sub_item in os.scandir(image_path):
             sub_item_path = image_path.joinpath(sub_item.name)
             if sub_item_path.is_dir() is False:
                 continue
+            # Validate `Version` directory structure
             if sub_item_path.joinpath('Dockerfile').exists() is False:
                 raise ValueError(f"Invalid file strucutre for {path}/{sub_item.name}. Missing Dockerfile")
             sub_paths.append(sub_item_path.name)
         versions = [Version(f'{path}/{sub_path}', [sub_path]) for sub_path in sub_paths]
-        for alias_new in aliases:
+
+        # Add aliases to list of tags each `Version` represents
+        aliases = info_json['aliases']
+        for alias_new in aliases:  # The tag that is not defined with a directory name
+            # The tag defined by a directory name
             alias_prev = aliases[alias_new]
             for version in versions:
                 if version.name == alias_prev:
@@ -53,7 +69,7 @@ class Image:
         return [
             {
                 'human-name': f'{self.name}: {version.tags[0]}' if len(self.versions) > 1 else self.name,
-                'path': os.path.join('images', version.path),
+                'path': str(ROOT_IMAGES_PATH.joinpath(version.path).relative_to(ROOT_PATH)),
                 'image-name': self.image_name,
                 'tags': ','.join(version.tags),
             }
@@ -61,11 +77,11 @@ class Image:
         ]
 
 
-out = []
-for sub_dir in os.scandir(root_path):
-    if root_path.joinpath(sub_dir.name, 'info.json').exists() is False:
-        continue
-    out.extend(Image.from_path(sub_dir.name).as_out())
+if __name__ == '__main__':
+    out = []
+    for sub_dir in os.scandir(ROOT_IMAGES_PATH):
+        if ROOT_IMAGES_PATH.joinpath(sub_dir.name, 'info.json').exists() is False:
+            continue
+        out.extend(Image.from_path(sub_dir.name).as_out())
 
-
-print(json.dumps({'include': out}))
+    print(json.dumps({'include': out}))
